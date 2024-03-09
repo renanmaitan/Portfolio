8077accf209a54a60d85ea9d31ba4c022999ab02
Renan Maitan <renanmaitan12@gmail.com>
2024-03-09 01:15:00 -0300

    nav-active-route

--- src/_components/nav-bar/nav-bar.component.ts
+++ src/_components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    RouterLinkActive
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
