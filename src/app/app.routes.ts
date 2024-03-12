6398bd72285e6ef47d3ade69d685b07832100fad
Renan Maitan <renanmaitan12@gmail.com>
2024-03-11 22:29:25 -0300

    diretory

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../pages/home/home.component';
 import { SkillsComponent } from '../pages/skills/skills.component';
+import { ProjectsComponent } from '../pages/projects/projects.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'skills', component: SkillsComponent}
+    {path: 'skills', component: SkillsComponent},
+    {path: 'projects', component: ProjectsComponent},
 ];
