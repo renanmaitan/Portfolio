649b96564488735b518c3f4df90789fd99a7c683
Renan Maitan <renanmaitan12@gmail.com>
2024-03-14 19:36:38 -0300

    changed to db file

---
+++ src/db/skills.ts
@@ -0,0 +1,45 @@
+export const skills = [
+  {
+    name: 'Electron',
+    src: 'assets/imgs/skills/electron-icon.png',
+    redirect: true,
+    url: '/projects/electron-certificate',
+  },
+  {
+    name: 'Flask',
+    src: 'assets/imgs/skills/flask-icon.png',
+    redirect: true,
+    url: '/projects/regnacraft',
+  },
+  {
+    name: 'Angular',
+    src: 'assets/imgs/skills/angular-icon.png',
+    redirect: true,
+    url: '/projects/portfolio',
+  },
+  {
+    name: 'React Native',
+    src: 'assets/imgs/skills/react-native-icon.png',
+    redirect: true,
+    url: '/projects',
+  },
+];
+export const languages = [
+  {
+    name: 'JavaScript',
+    src: 'assets/imgs/skills/js-icon.png',
+    redirect: false,
+  },
+  {
+    name: 'Python',
+    src: 'assets/imgs/skills/python-icon.png',
+    redirect: false,
+  },
+  {
+    name: 'Typescript',
+    src: 'assets/imgs/skills/typescript-icon.png',
+    redirect: false,
+  },
+  { name: 'C/C++', src: 'assets/imgs/skills/cpp-icon.png', redirect: false },
+  { name: 'Java', src: 'assets/imgs/skills/java-icon.png', redirect: false },
+];
--- src/pages/skills/skills.component.ts
+++ src/pages/skills/skills.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SkillComponent } from '../../_components/skill/skill.component';
+import { skills, languages } from '../../db/skills';
 
 @Component({
   selector: 'app-skills',
@@ -11,17 +12,6 @@ import { SkillComponent } from '../../_components/skill/skill.component';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-  skills = [
-    {name: 'Electron', src: 'assets/imgs/skills/electron-icon.png', redirect: true, url: '/home'},
-    {name: 'Flask', src: 'assets/imgs/skills/flask-icon.png', redirect: true, url: '/skills'},
-    {name: 'Angular', src: 'assets/imgs/skills/angular-icon.png', redirect: true},
-    {name: 'React Native', src: 'assets/imgs/skills/react-native-icon.png', redirect: true},
-  ];
-  languages = [
-    {name: 'JavaScript', src: 'assets/imgs/skills/js-icon.png', redirect: false},
-    {name: 'Python', src: 'assets/imgs/skills/python-icon.png', redirect: false},
-    {name: 'Typescript', src: 'assets/imgs/skills/typescript-icon.png', redirect: false},
-    {name: 'C/C++', src: 'assets/imgs/skills/cpp-icon.png', redirect: false},
-    {name: 'Java', src: 'assets/imgs/skills/java-icon.png', redirect: false},
-  ];
+  skills = skills;
+  languages = languages;
 }
