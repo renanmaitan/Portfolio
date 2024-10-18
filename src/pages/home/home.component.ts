7b1c84076ef875308e6c2f2dea83c03e6632c14b
Renan Maitan <renanmaitan12@gmail.com>
2024-10-17 21:21:34 -0300

    dynamic semester

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -11,5 +11,22 @@ import { RouterModule } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  currentStatus: string;
 
+  constructor() {
+    this.currentStatus = this.calculateSemester();
+  }
+
+  calculateSemester(): string {
+    const startYear = 2021;
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentHalf = currentMonth > 6 ? 2 : 1;
+    const currentSemester = (currentYear - startYear) * 2 + currentHalf;
+    if (currentSemester>10){
+      return "sou formado em Engenharia da Computação pela FACENS"
+    }
+    return `sou estudante do ${(currentYear - startYear) * 2 + currentHalf}º semestre de Engenharia da Computação na FACENS`;
+  }
 }
